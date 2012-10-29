552f217a86e9b66093a0f50cc19d47a840685ff1
Anand Prakash <anand.prakash@gmail.com>
2012-10-28 23:23:57 -0700

    bumping version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject timewarrior/clj-facebook-graph "0.5.0"
+(defproject timewarrior/clj-facebook-graph "0.5.1"
   :description "A Clojure client for the Facebook Graph API."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.1"]
                  [ring/ring-core "1.1.5"]
                  [clj-http "0.5.6"]
-                 [timewarrior/clj-oauth2 "0.5.0"]]
+                 [timewarrior/clj-oauth2 "0.5.1"]]
   :dev-dependencies [[ring/ring-devel "1.1.5"]
                      [ring/ring-jetty-adapter "1.1.5"]
                      [compojure "1.1.3"]]
